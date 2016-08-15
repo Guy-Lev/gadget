829074ea35c443c8582e9257fa5f61141da2ef55
Rotem Yaari <vmalloc@gmail.com>
2016-08-15 13:55:38 +0300

    Basic landing page design

--- webapp/app/routes/index.js
+++ webapp/app/routes/index.js
@@ -1,11 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
-        return {features: [
-            {name: "Font Awesome", ok: true},
-            {name: "Ember.js", ok: true},
-            {name: "Bootstrap", ok: true}
-        ]};
-    }
 });
